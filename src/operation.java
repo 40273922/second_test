f6368bf6beebceedbd63f770634ad754a7c76891
jinpei <2145619745@qq.com>
2022-09-01 12:35:28 +0800

    second test

---
+++ src/operation.java
@@ -0,0 +1,24 @@
+/**
+ * @Program: operation
+ * @description:
+ * @author: xie mu
+ * @create: -09-01 09:39
+ **/
+public class operation {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int y;
+        y = i++ + 1;
+        i = i++;
+        y++;
+        long u = 0x10000000;
+        System.out.println("i=" + i);
+        System.out.println("y=" + y);
+        System.out.println("u=" + u);
+
+    }
+
+
+
+    }
